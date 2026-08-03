731800ed5b9df6f99ef5a76fa23f724220d6708d
Sadiq Syed <syedsadiq1304@gmail.com>
2026-08-03 10:54:42 +0530

    Add task menu and view tasks feature

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-print("Welcome to Student Task Manager!")
+print("===== Student Task Manager =====")
 
 name = input("Enter your name: ")
 
 print("\nHello", name)
-print("Welcome to your task manager!")
 
 tasks = []
 
-task = input("\nEnter a task: ")
+while True:
+    print("\n===== MENU =====")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Exit")
 
-tasks.append(task)
+    choice = input("Enter your choice: ")
 
-print("\nTask added successfully!")
+    if choice == "1":
+        task = input("Enter a task: ")
+        tasks.append(task)
+        print("Task added successfully!")
 
-print("\nYour Tasks:")
+    elif choice == "2":
+        if len(tasks) == 0:
+            print("No tasks available.")
 
-for task in tasks:
-    print("-", task)
+        else:
+            print("\nYour Tasks:")
+
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}")
+
+    elif choice == "3":
+        print("Thank you for using Student Task Manager!")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
