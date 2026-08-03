8d1dbc36160f575aa45e3ca8630f78ff93b9674d
Sadiq Syed <syedsadiq1304@gmail.com>
2026-08-03 10:43:08 +0530

    Add task creation feature

--- main.py
+++ main.py
@@ -2,5 +2,18 @@ print("Welcome to Student Task Manager!")
 
 name = input("Enter your name: ")
 
-print("Hello", name)
+print("\nHello", name)
 print("Welcome to your task manager!")
+
+tasks = []
+
+task = input("\nEnter a task: ")
+
+tasks.append(task)
+
+print("\nTask added successfully!")
+
+print("\nYour Tasks:")
+
+for task in tasks:
+    print("-", task)
